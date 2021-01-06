85e33868421acef954767287c793e9e26b92af53
zhoushijia <zhoushijia@foxmail.com>
2021-01-06 18:45:21 +0800

    使用mime包对服务器案例优化

--- 0106-node/6-响应页面内容.js
+++ 0106-node/6-响应页面内容.js
@@ -1,10 +1,12 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+// 引入第三方资源包
+const mime = require('mime')
 
 const server = http.createServer()
 server.on('request', (req, res) => {
-  //   res.setHeader('Content-Type', 'text/html;charset=utf-8')
+  //   res.setHeader('Content-Type', `${mime.getType(fpath)};charset=utf-8`)
 
   // 拼接请求路径
   //   const fpath = path.join(__dirname, req.url)
@@ -15,6 +17,8 @@ server.on('request', (req, res) => {
   } else {
     fpath = path.join(__dirname, 'timer', req.url)
   }
+  res.setHeader('Content-Type', `${mime.getType(fpath)};charset=utf-8`)
+
   fs.readFile(fpath, 'utf8', (err, dataStr) => {
     if (err) return res.end('404 NOT FOUND!')
     // 要设置响应头
