287679d76f2e114cbcde1086c74ae5bc1953b5a9
zhoushijia <zhoushijia@foxmail.com>
2021-01-13 11:02:59 +0800

    建立mysql连接

---
+++ 0113-node/api_server/db/db.js
@@ -0,0 +1,18 @@
+// 导入myqsl 连接mysql数据库
+const mysql = require('mysql')
+
+// 创建数据库连接池对象
+const db = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'root',
+  database: 'big_event'
+})
+
+// 测试用
+db.query('select * from users', (err, results) => {
+  if (err) throw err
+  console.log(results)
+})
+// 导出数据库
+module.exports = db
