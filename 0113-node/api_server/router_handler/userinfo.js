2d80a4a493e9f34a308a175b37584b7a27d0209e
zhoushijia <zhoushijia@foxmail.com>
2021-01-14 11:07:10 +0800

    获取用户信息

--- 0113-node/api_server/router_handler/userinfo.js
+++ 0113-node/api_server/router_handler/userinfo.js
@@ -1,6 +1,19 @@
+// 导入sql
+const db = require('../db/db')
+
 // 获取用户信息
 exports.getinfo = (req, res) => {
-  res.send('ok')
+  const sqlStr = 'select id,username,nickname,email,user_pic from users where username=?'
+  db.query(sqlStr, req.user.username, (err, results) => {
+    //   sql错误
+    if (err) return res.cc(err)
+    if (results.length !== 1) return res.cc('用户信息获取失败')
+    res.send({
+      status: 0,
+      msg: '获取用户信息成功',
+      data: results[0]
+    })
+  })
 }
 
 // 更新用户信息
